4f0b1ea3d91ecaaf9024e17baa4e95f643a3abe9
THANGTA\tangu <tanguyentruongthang@gmail.com>
2024-11-30 14:32:02 -0700

    set up theme for dark and light modes

--- frontend/src/components/TestComponent.tsx
+++
@@ -1,8 +0,0 @@
-import { useTheme } from "../context/theme.context";
-
-const TestComponent = () => {
-  const { isDarkMode, toggleTheme } = useTheme();
-  return <div></div>;
-};
-
-export default TestComponent;
