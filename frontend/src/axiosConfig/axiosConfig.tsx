fcea39d103f9579ac3c315f7ffc0d3de8f1bbdb9
THANGTA\tangu <tanguyentruongthang@gmail.com>
2025-02-01 22:01:38 -0700

    updated axiosConfig with post helper method

--- frontend/src/axiosConfig/axiosConfig.tsx
+++ frontend/src/axiosConfig/axiosConfig.tsx
@@ -1,20 +1,30 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
+// 1. Create axios instance with default configuration
 const axiosClient = axios.create({
   baseURL: "http://localhost:8080",
-  // baseURL: "https://host.docker.internal:32769",
   headers: {
     "Content-Type": "application/json",
     Accept: "text/plain",
   },
   timeout: 10000,
-  // withCredentials: true,
 });
 
-export const get = <T, R = AxiosResponse<T>, D = unknown>(
+// 2. GET helper method
+export const get = <T,>(
   url: string,
-  config?: AxiosRequestConfig<D>
-): Promise<R> => {
-  return axiosClient.get<T, R, D>(url, config);
+  config?: AxiosRequestConfig
+): Promise<AxiosResponse<T>> => {
+  return axiosClient.get<T>(url, config);
 };
+
+// 3. POST helper method
+export const post = <T, D = unknown>(
+  url: string,
+  data: D,
+  config?: AxiosRequestConfig
+): Promise<AxiosResponse<T>> => {
+  return axiosClient.post<T>(url, data, config);
+};
+
 export default axiosClient;
