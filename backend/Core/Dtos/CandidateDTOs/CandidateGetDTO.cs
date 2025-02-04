0e9c3cc0d3a6f0d21ddfc2157ad1ef0e5fa9833e
THANGTA\tangu <tanguyentruongthang@gmail.com>
2025-02-04 16:51:44 -0700

    added ID for download endpoing - added Company name to display it on application page

--- backend/Core/Dtos/CandidateDTOs/CandidateGetDTO.cs
+++ backend/Core/Dtos/CandidateDTOs/CandidateGetDTO.cs
@@ -5,6 +5,7 @@ namespace backend.Core.Dtos.CandidateDTOs
 {
     public class CandidateGetDTO
     {
+        public long ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
@@ -12,5 +13,6 @@ namespace backend.Core.Dtos.CandidateDTOs
         public string CoverLetter { get; set; }
         public string JobName { get; set; }
         public long JobId { get; set; }
+        public string CompanyName { get; set; }
     }
 }
