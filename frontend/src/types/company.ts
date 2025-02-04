c9b36b9e7f77f54977a00a5dd19b1ba3ebb8beac
THANGTA\tangu <tanguyentruongthang@gmail.com>
2025-02-03 17:13:43 -0700

    removed comments

--- frontend/src/types/company.ts
+++ frontend/src/types/company.ts
@@ -1,11 +1,9 @@
-// Enum for company sizes that matches backend
 export enum CompanySize {
   Small = "Small",
   Medium = "Medium",
   Large = "Large",
 }
 
-// Company interface
 export interface Company {
   id: number;
   name: string;
@@ -13,14 +11,12 @@ export interface Company {
   jobs: Job[];
 }
 
-// Job interface
 export interface Job {
   id: number;
   title: string;
   jobLevel: string;
 }
 
-// Request type for creating a company
 export interface CreateCompanyRequest {
   name: string;
   size: CompanySize;
