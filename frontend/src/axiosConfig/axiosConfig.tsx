8f6d407c3ab4a816ec202b690cd033e7d0d0e462
THANGTA\tangu <tanguyentruongthang@gmail.com>
2025-02-05 11:51:55 -0700

    added quick fix for using 404 when fetching list and there is no data found

--- frontend/src/axiosConfig/axiosConfig.tsx
+++ frontend/src/axiosConfig/axiosConfig.tsx
@@ -10,7 +10,18 @@ const axiosClient = axios.create({
   timeout: 10000,
 });
 
-// 2. GET helper method
+// Response interceptor to quickly fix the mistake of using 404 status code when the list is empty
+axiosClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 404 && error.config?.method === "get") {
+      return Promise.resolve({ data: [] });
+    }
+    return Promise.reject(error);
+  }
+);
+
+// GET helper method
 export const get = <T,>(
   url: string,
   config?: AxiosRequestConfig
@@ -18,7 +29,7 @@ export const get = <T,>(
   return axiosClient.get<T>(url, config);
 };
 
-// 3. POST helper method
+// POST helper method
 export const post = <T, D = unknown>(
   url: string,
   data: D,
